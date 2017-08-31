9dc1c9871422f2238d2f6ba3cb07410d7e347d38
howiehub <1115096801@qq.com>
2017-08-31 15:13:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-echo qqq;
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>qaq</title>
+</head>
+<body>
+我叫qaq 。
+</body>
+</html>
